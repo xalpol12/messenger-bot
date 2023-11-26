8b66967cd17007fa5f1ccdff960f104730504185
Dawid Stawiszyński <xalpol12@wp.pl>
2023-11-26 15:19:18 +0100

    init commit

---
+++ src/main/java/com/xalpol12/messengerbot/MessengerBotApplication.java
@@ -0,0 +1,13 @@
+package com.xalpol12.messengerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xalpol12/messengerbot/MessengerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xalpol12.messengerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
