79a72d49b46b6062dbc79983b542cd8d904517f5
Dawid Stawiszyński <xalpol12@wp.pl>
2023-12-30 13:10:47 +0100

    add logger to exception handlers

--- src/main/java/com/xalpol12/messengerbot/crud/exception/CrudExceptionHandler.java
+++ src/main/java/com/xalpol12/messengerbot/crud/exception/CrudExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.xalpol12.messengerbot.crud.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.postgresql.util.PSQLException;
 import org.postgresql.util.PSQLState;
 import org.springframework.http.HttpStatus;
@@ -7,26 +8,36 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class CrudExceptionHandler {
 
     @ExceptionHandler(ImageAccessException.class)
     public ResponseEntity<String> handleImageAccessException(ImageAccessException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        String message = extractMessageAndLog(e);
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ImageNotFoundException.class)
     public ResponseEntity<String> handleImageNotFoundException(ImageNotFoundException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        String message = extractMessageAndLog(e);
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(PSQLException.class)
     public ResponseEntity<String> handlePSQLException(PSQLException e) throws PSQLException {
         if (e.getSQLState().equals(PSQLState.UNIQUE_VIOLATION.getState())) {
             String errorMessage = "Provided custom uri that already exists in a database";
+            log.error("Encountered {} with message: {}", e.getClass().getName(), errorMessage);
             return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
         } else {
             throw e;
         }
+    }
+
+    private String extractMessageAndLog(RuntimeException e) {
+        String message = e.getMessage();
+        log.error("Encountered {} with message: {}", e.getClass().getName(), message);
+        return message;
     }
 }
--- src/main/java/com/xalpol12/messengerbot/messengerplatform/exception/MessengerPlatformExceptionHandler.java
+++ src/main/java/com/xalpol12/messengerbot/messengerplatform/exception/MessengerPlatformExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.xalpol12.messengerbot.messengerplatform.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class MessengerPlatformExceptionHandler {
 
@@ -12,12 +14,20 @@ public class MessengerPlatformExceptionHandler {
             IncorrectWebhookModeException.class,
             IncorrectTokenException.class
     })
-    public ResponseEntity<?> handleWebhookVerificationExceptions(RuntimeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+    public ResponseEntity<?> handleWebhookVerificationExceptions(RuntimeException e) {
+        String message = extractMessageAndLog(e);
+        return new ResponseEntity<>(message, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(value = {RequestSignatureValidationException.class})
-    public ResponseEntity<?> handleHashValidationException(RuntimeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> handleHashValidationException(RuntimeException e) {
+        String message = extractMessageAndLog(e);
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    private String extractMessageAndLog(RuntimeException e) {
+        String message = e.getMessage();
+        log.error("Encountered {} with message: {}", e.getClass().getName(), message);
+        return message;
     }
 }
