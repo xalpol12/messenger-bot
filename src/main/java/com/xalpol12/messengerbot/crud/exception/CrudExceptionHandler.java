a03849c8f7c809d349eafcbad25c793e6c1b82da
Dawid Stawiszyński <xalpol12@wp.pl>
2023-12-30 12:01:27 +0100

    add psql unique exception handling method, create tests for CrudExceptionHandler

--- src/main/java/com/xalpol12/messengerbot/crud/exception/CrudExceptionHandler.java
+++ src/main/java/com/xalpol12/messengerbot/crud/exception/CrudExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.xalpol12.messengerbot.crud.exception;
 
+import org.postgresql.util.PSQLException;
+import org.postgresql.util.PSQLState;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,12 +11,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CrudExceptionHandler {
 
     @ExceptionHandler(ImageAccessException.class)
-    public ResponseEntity<?> handleImageAccessException(ImageAccessException e) {
+    public ResponseEntity<String> handleImageAccessException(ImageAccessException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ImageNotFoundException.class)
-    public ResponseEntity<?> handleImageNotFoundException(ImageNotFoundException e) {
+    public ResponseEntity<String> handleImageNotFoundException(ImageNotFoundException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(PSQLException.class)
+    public ResponseEntity<String> handlePSQLException(PSQLException e) throws PSQLException {
+        if (e.getSQLState().equals(PSQLState.UNIQUE_VIOLATION.getState())) {
+            String errorMessage = "Provided custom uri that already exists in a database";
+            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        } else {
+            throw e;
+        }
     }
 }
---
+++ src/test/java/com/xalpol12/messengerbot/crud/exception/CrudExceptionHandlerTest.java
@@ -0,0 +1,52 @@
+package com.xalpol12.messengerbot.crud.exception;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.postgresql.util.PSQLException;
+import org.postgresql.util.PSQLState;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CrudExceptionHandlerTest {
+
+    private static CrudExceptionHandler exceptionHandler;
+
+    @BeforeAll
+    public static void setup() {
+        exceptionHandler = new CrudExceptionHandler();
+    }
+
+    @Test
+    public void handleImageAccessException() {
+        String message = "Exception message";
+        ImageAccessException exception = new ImageAccessException(message);
+
+        ResponseEntity<String> response = exceptionHandler.handleImageAccessException(exception);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(message, response.getBody());
+    }
+
+    @Test
+    public void handleImageNotFoundException() {
+        String message = "Exception message";
+        ImageNotFoundException exception = new ImageNotFoundException(message);
+
+        ResponseEntity<String> response = exceptionHandler.handleImageNotFoundException(exception);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(message, response.getBody());
+    }
+
+    @Test
+    public void handlePSQLException_uniqueConstraintExceptionViolated() throws PSQLException {
+        PSQLState uniqueViolation = PSQLState.UNIQUE_VIOLATION;
+        PSQLException exception = new PSQLException("Message", uniqueViolation);
+
+        ResponseEntity<String> response = exceptionHandler.handlePSQLException(exception);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+}
