e01f7080372dbca7e0468ba90dc3a00fd5d85211
Dawid Stawiszyński <xalpol12@wp.pl>
2023-12-31 14:22:26 +0100

    enable feign clients application-wide

--- src/main/java/com/xalpol12/messengerbot/MessengerBotApplication.java
+++ src/main/java/com/xalpol12/messengerbot/MessengerBotApplication.java
@@ -2,10 +2,12 @@ package com.xalpol12.messengerbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
 public class MessengerBotApplication {
 
     public static void main(String[] args) {
