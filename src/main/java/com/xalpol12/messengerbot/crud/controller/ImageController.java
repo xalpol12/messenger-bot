174e6595e51b1a6ded6ea8d7af004a2979797ead
Dawid Stawiszyński <xalpol12@wp.pl>
2024-01-04 16:21:55 +0100

    add ImageController swagger annotations

--- src/main/java/com/xalpol12/messengerbot/crud/controller/ImageController.java
+++ src/main/java/com/xalpol12/messengerbot/crud/controller/ImageController.java
@@ -6,6 +6,7 @@ import com.xalpol12.messengerbot.crud.model.dto.image.ImageUploadDetails;
 import com.xalpol12.messengerbot.crud.service.ImageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,7 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.net.URI;
 import java.util.List;
 
-@Tag(name = "Images API")
+@Tag(name = "Images API", description = "API for managing images")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -34,15 +35,22 @@ public class ImageController {
     private final ImageService imageService;
 
     @Operation(
-            summary = "",
-            description = "",
-            tags = {})
-    @ApiResponses( value = {
-            @ApiResponse(responseCode = "200", description = ""),
+            summary = "Display image data",
+            description = "Sends the image body for display in web browser as a page. " +
+                    "Caches accessed entity for faster retrieval.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the message body for given image ID"),
+            @ApiResponse(responseCode = "404", description = "Image with given ID not found in the database"),
     })
     @GetMapping(ImagePath.ROOT + "/{uri}")
     public ResponseEntity<byte[]> displayImageData(
-            @PathVariable("uri") @Parameter(name = "uri", description = "", examples = {}) String uri) {
+            @Parameter(
+                    name = "uri",
+                    description = "Unique Image entity identifier",
+                    examples = {
+                            @ExampleObject("558e89c8-79d2-4312-9750-73c24afe253e"),
+                            @ExampleObject("munch-showcase")})
+            @PathVariable("uri") String uri) {
         Image image = imageService.getImage(uri);
 
         MediaType mediaType = MediaType.parseMediaType(image.getType());
@@ -54,8 +62,22 @@ public class ImageController {
                 .body(image.getData());
     }
 
+    @Operation(
+            summary = "Download image",
+            description = "Redirects browser to download image with given ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the message for given image ID"),
+            @ApiResponse(responseCode = "404", description = "Image with given ID not found in the database"),
+    })
     @GetMapping(ImagePath.ROOT + "/{uri}/download")
-    public ResponseEntity<byte[]> getImageData(@PathVariable("uri") String uri) {
+    public ResponseEntity<byte[]> getImageData(
+            @Parameter(
+                    name = "uri",
+                    description = "Unique Image entity identifier",
+                    examples = {
+                            @ExampleObject("558e89c8-79d2-4312-9750-73c24afe253e"),
+                            @ExampleObject("munch-showcase")})
+            @PathVariable("uri") String uri) {
         Image image = imageService.getImage(uri);
         MediaType mediaType = MediaType.parseMediaType(image.getType());
 
@@ -69,6 +91,12 @@ public class ImageController {
                 .body(image.getData());
     }
 
+    @Operation(
+            summary = "Return list of all images",
+            description = "Returns all images info in the form of ImageDTO list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved all available images"),
+    })
     @GetMapping(ImagePath.ROOT + "s")
     public ResponseEntity<List<ImageDTO>> getAllImages() {
         log.trace("GET /images called");
@@ -77,41 +105,115 @@ public class ImageController {
         return ResponseEntity.ok(images);
     }
 
+    @Operation(
+            summary = "Upload image",
+            description = "Saves new image entity in the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully saved new entity in the database"),
+            @ApiResponse(responseCode = "400", description = "Couldn't access image data bytes"),
+    }) // TODO: check other exceptions in this endpoint
     @PostMapping(path = ImagePath.ROOT, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> uploadImage(@RequestPart ImageUploadDetails fileDetails,
-                                         @RequestPart MultipartFile file) {
+    public ResponseEntity<?> uploadImage(
+            @Parameter(name = "file details", description = "ImageUploadDetails object")
+            @RequestPart ImageUploadDetails fileDetails,
+            @Parameter(name = "file", description = "MultipartFile object, actual image file")
+            @RequestPart MultipartFile file) {
         URI savedLocation = imageService.uploadImage(fileDetails, file);
         log.trace("POST /images/upload called for file with URI: {}", savedLocation);
         return ResponseEntity.created(savedLocation).build();
     }
 
+    @Operation(
+            summary = "Delete image",
+            description = "Deletes image with given ID from the database. " +
+                    "Also deletes all ScheduledImage entities that were associated with this image.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successfully deleted the entity with given ID from the database"),
+            @ApiResponse(responseCode = "404", description = "Image with given ID not found in the database"),
+    })
     @DeleteMapping(ImagePath.ROOT + "/{uri}")
-    public ResponseEntity<?> deleteImage(@PathVariable("uri") String uri) {
+    public ResponseEntity<?> deleteImage(
+            @Parameter(
+                    name = "uri",
+                    description = "Unique Image entity identifier",
+                    examples = {
+                            @ExampleObject("558e89c8-79d2-4312-9750-73c24afe253e"),
+                            @ExampleObject("munch-showcase")})
+            @PathVariable("uri") String uri) {
         log.trace("DELETE image/{uri} called for entity with uri: {}", uri);
         imageService.deleteImage(uri);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Update image",
+            description = "Replaces old entity with the entity created from provided details, retaining the same ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted entity with given ID in the database"),
+            @ApiResponse(responseCode = "400", description = "Couldn't access image data bytes"),
+            @ApiResponse(responseCode = "404", description = "Image with given ID not found in the database"),
+    })
     @PutMapping(value = ImagePath.ROOT + "/{uri}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> updateImage(@PathVariable("uri") String uri,
-                                         @RequestPart ImageUploadDetails fileDetails,
-                                         @RequestPart MultipartFile file) {
+    public ResponseEntity<?> updateImage(
+            @Parameter(
+                    name = "uri",
+                    description = "Unique Image entity identifier",
+                    examples = {
+                            @ExampleObject("558e89c8-79d2-4312-9750-73c24afe253e"),
+                            @ExampleObject("munch-showcase")})
+            @PathVariable("uri") String uri,
+            @Parameter(name = "file details", description = "ImageUploadDetails object")
+            @RequestPart ImageUploadDetails fileDetails,
+            @Parameter(name = "file", description = "MultipartFile object, actual image file")
+            @RequestPart MultipartFile file) {
         log.trace("PUT image/{uri} called for entity with uri: {}", uri);
         imageService.updateImage(uri, fileDetails, file);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(
+            summary = "Patch image details",
+            description = "Modifies entity with the details provided in the request, " +
+                    "null request fields are not processed")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted entity with given ID in the database"),
+            @ApiResponse(responseCode = "404", description = "Image with given ID not found in the database"),
+    })
     @PatchMapping(ImagePath.ROOT + "/{uri}/details")
-    public ResponseEntity<?> updateImageDetails(@PathVariable("uri") String uri,
-                                                @RequestBody ImageUploadDetails newDetails) {
+    public ResponseEntity<?> updateImageDetails(
+            @Parameter(
+                    name = "uri",
+                    description = "Unique Image entity identifier",
+                    examples = {
+                            @ExampleObject("558e89c8-79d2-4312-9750-73c24afe253e"),
+                            @ExampleObject("munch-showcase")})
+            @PathVariable("uri") String uri,
+            @Parameter(name = "file details", description = "ImageUploadDetails object")
+            @RequestBody ImageUploadDetails newDetails) {
         log.trace("PATCH image/{uri} image details called for entity with uri: {}", uri);
         imageService.patchImageDetails(uri, newDetails);
         return ResponseEntity.ok().build();
     }
 
-    @PatchMapping(ImagePath.ROOT + "/{uri}/data")
-    public ResponseEntity<?> updateImageData(@PathVariable("uri") String uri,
-                                             @RequestPart MultipartFile file) {
+    @Operation(
+            summary = "Patch image data",
+            description = "Modifies entity with the image data provided in the request")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted entity with given ID in the database"),
+            @ApiResponse(responseCode = "400", description = "Couldn't access image data bytes"),
+            @ApiResponse(responseCode = "404", description = "Image with given ID not found in the database"),
+    })
+    @PatchMapping(value = ImagePath.ROOT + "/{uri}/data", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> updateImageData(
+            @Parameter(
+                    name = "uri",
+                    description = "Unique Image entity identifier",
+                    examples = {
+                            @ExampleObject("558e89c8-79d2-4312-9750-73c24afe253e"),
+                            @ExampleObject("munch-showcase")})
+            @PathVariable("uri") String uri,
+            @Parameter(name = "file", description = "MultipartFile object, actual image file")
+            @RequestPart MultipartFile file) {
         log.trace("PATCH image/{uri} image data called for entity with uri: {}", uri);
         imageService.patchImageData(uri, file);
         return ResponseEntity.ok().build();
