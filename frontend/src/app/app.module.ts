0c92e587c149672698f2c99a3e86c73f0f83f8ba
Dawid Stawiszyński <xalpol12@wp.pl>
2024-01-06 19:30:10 +0100

    add forms module to frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,19 @@ import {AppComponent} from "./app.component";
 import {FileUploadComponent} from "./components/file-upload/file-upload.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     FileUploadComponent,
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
