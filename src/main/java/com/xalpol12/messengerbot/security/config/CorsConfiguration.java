71ecca7ec86605da37e6535e0ddbe20a14c4423d
Dawid Stawiszyński <xalpol12@wp.pl>
2024-01-06 14:22:35 +0100

    disable cors config for local profile in backend

---
+++ src/main/java/com/xalpol12/messengerbot/security/config/CorsConfiguration.java
@@ -0,0 +1,25 @@
+package com.xalpol12.messengerbot.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("local")
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*").maxAge(3600);
+            }
+        };
+    }
+}
