316c89876f23d999b5843f6f46cb6e9f1b331d4a
Dawid Stawiszyński <xalpol12@wp.pl>
2024-01-07 11:56:53 +0100

    fix incorrect fileDetails key values

--- frontend/src/app/components/file-upload/image-entry/image-entry.component.ts
+++ frontend/src/app/components/file-upload/image-entry/image-entry.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ImageInfo} from "../../../models/image.model";
 import {NgIf} from "@angular/common";
 
