5eee004aeefdefb45d8c4f0608e2f53d9be50a0c
Dawid Stawiszyński <xalpol12@wp.pl>
2024-01-13 17:27:08 +0100

    add icon swap for responsive navbar layout

--- frontend/src/app/core/components/navbar/navbar.component.ts
+++ frontend/src/app/core/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {RouterLink, RouterOutlet} from "@angular/router";
 import {NgbCollapse} from "@ng-bootstrap/ng-bootstrap";
+import {MediaMatcher} from "@angular/cdk/layout";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
@@ -9,10 +11,26 @@ import {NgbCollapse} from "@ng-bootstrap/ng-bootstrap";
     RouterLink,
     RouterOutlet,
     NgbCollapse,
+    NgIf,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
   isMenuCollapsed = true;
+  isNavbarHidden: boolean;
+
+  private mediaMatcher: MediaQueryList;
+
+  constructor(private cdr: ChangeDetectorRef,
+              private mediaMatcherService: MediaMatcher) {
+    this.mediaMatcher = this.mediaMatcherService.matchMedia('(max-width: 991px)');
+    this.isNavbarHidden = this.mediaMatcher.matches;
+
+    this.mediaMatcher.addEventListener('change', (event) => {
+      this.isNavbarHidden = event.matches;
+      this.cdr.detectChanges();
+    })
+  }
 }
