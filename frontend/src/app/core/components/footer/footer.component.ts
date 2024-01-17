fad99cab47f361f94e14212b81a4bc1ce97ac76c
Dawid Stawiszyński <xalpol12@wp.pl>
2024-01-17 19:11:27 +0100

    change href to open in new tab

--- frontend/src/app/core/components/footer/footer.component.ts
+++ frontend/src/app/core/components/footer/footer.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
     <footer class="mt-3 text-center fw-lighter">
       <p>created by
         <a href="https://github.com/xalpol12"
+           target="_blank"
+           rel="noopener noreferrer"
            style="color: inherit; text-decoration: none"
            class="text-decoration-underline">
           xalpol12 <i class="bi-github"></i>
