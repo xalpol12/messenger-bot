887e56d122515e6dd50c316c5399a6adb1952604
Dawid Stawiszyński <xalpol12@wp.pl>
2024-01-17 09:39:58 +0100

    add footer.component to tracked files

---
+++ frontend/src/app/core/components/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  template: `
+    <footer class="mt-3 text-center fw-lighter">
+      <p>created by
+        <a href="https://github.com/xalpol12"
+           style="color: inherit; text-decoration: none"
+           class="text-decoration-underline">
+          xalpol12 <i class="bi-github"></i>
+        </a>
+      </p>
+    </footer>
+  `,
+  styles: []
+})
+export class FooterComponent {
+}
