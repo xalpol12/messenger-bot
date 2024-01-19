feffc03fcbc4f43b417d57a52ce752702855a6c7
Dawid Stawiszyński <xalpol12@wp.pl>
2024-01-19 14:07:05 +0100

    POWERFUL ExceptionUtils getErrorCode method refactor

--- src/main/java/com/xalpol12/messengerbot/crud/exception/utils/ExceptionUtils.java
+++ src/main/java/com/xalpol12/messengerbot/crud/exception/utils/ExceptionUtils.java
@@ -7,25 +7,30 @@ import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 public class ExceptionUtils {
 
+    private static final Map<Class<? extends Exception>, String> ERROR_CODE_MAP = new HashMap<>();
+
+    static {
+        ERROR_CODE_MAP.put(ImageAccessException.class, "IMAGE_ACCESS_EXCEPTION");
+        ERROR_CODE_MAP.put(IllegalArgumentException.class, "IMAGE_DIMENSIONS_ACCESS_ERROR");
+        ERROR_CODE_MAP.put(ImageNotFoundException.class, "IMAGE_NOT_FOUND");
+        ERROR_CODE_MAP.put(ScheduledMessageNotFoundException.class, "SCHEDULED_MESSAGE_NOT_FOUND");
+        ERROR_CODE_MAP.put(MethodArgumentNotValidException.class, "METHOD_ARGUMENT_NOT_VALID");
+        ERROR_CODE_MAP.put(ConstraintViolationException.class, "CONSTRAINT_VIOLATED");
+    }
+
     public static String getErrorCode(Exception ex) {
-        if (ex instanceof ImageAccessException) {
-            return "IMAGE_ACCESS_EXCEPTION";
-        } else if (ex instanceof IllegalArgumentException) {
-            return "IMAGE_DIMENSIONS_ACCESS_ERROR";
-        } else if (ex instanceof ImageNotFoundException) {
-            return "IMAGE_NOT_FOUND";
-        } else if (ex instanceof ScheduledMessageNotFoundException) {
-            return "SCHEDULED_MESSAGE_NOT_FOUND";
-        } else if (ex instanceof MethodArgumentNotValidException) {
-            return "METHOD_ARGUMENT_NOT_VALID";
-        } else if (ex instanceof ConstraintViolationException) {
-            return "CONSTRAINT_VIOLATED";
-        };
-        return "UNKNOWN_ERROR_CODE";
-    };
+        return ERROR_CODE_MAP.entrySet().stream()
+                .filter(entry -> entry.getKey().isInstance(ex))
+                .findFirst()
+                .map(Map.Entry::getValue)
+                .orElse("UNKNOWN_ERROR_CODE");
+    }
 
     public static String getMessageAndLog(RuntimeException e) {
         String message = e.getMessage();
