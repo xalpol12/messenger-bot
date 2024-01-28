3ef5efe0b1170e8bc1087497289be186e2f8df4f
Dawid Stawiszyński <xalpol12@wp.pl>
2024-01-28 09:05:27 +0100

    add image-input border and some more clarity

--- frontend/src/app/features/components/file-upload/image-list/image-entry/image-details/image-details.component.ts
+++ frontend/src/app/features/components/file-upload/image-list/image-entry/image-details/image-details.component.ts
@@ -2,14 +2,16 @@ import {Component, Input} from '@angular/core';
 import {ImageInfo} from "../../../../../models/image.model";
 import {DatePipe} from "@angular/common";
 import {SizeFormatPipe} from "../../../../../../shared/pipes/size-format.pipe";
+import {ThumbnailComponent} from "../../../thumbnail/thumbnail.component";
 
 @Component({
   selector: 'app-image-details',
   standalone: true,
-  imports: [
-    DatePipe,
-    SizeFormatPipe
-  ],
+    imports: [
+        DatePipe,
+        SizeFormatPipe,
+        ThumbnailComponent
+    ],
   templateUrl: './image-details.component.html',
   styleUrl: './image-details.component.css'
 })
